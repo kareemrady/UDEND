981c89f390acfd3f1e534e45a2a0fb7b63e3474a
= <=>
2020-04-11 23:17:44 +0000

    ADDED PRI KEYS as well as constraints on tables

--- Project1/.ipynb_checkpoints/sql_queries-checkpoint.py
+++ Project1/.ipynb_checkpoints/sql_queries-checkpoint.py
@@ -8,54 +8,59 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (time_stamp bigint, \
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplays_id SERIAL PRIMARY KEY, time_start bigint, \
                                                                   user_id int, level varchar,\
                                                                   song_id varchar, artist_id varchar,\
                                                                   session_id int, location varchar, \
-                                                                  user_agent varchar)
+                                                                  user_agent varchar \
+                                                                  )
 """)
 
-user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar,\
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar,\
                                                            last_name varchar, gender char, level varchar)
 """)
 
-song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, \
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, \
                                                            year int, duration decimal)
 """)
 
-artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, \
+artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, location varchar, \
                                                                latitude decimal , longitude decimal)
 """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time (time_stamp bigint, hour int, day int, weekofyear int, \
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (time_start bigint, hour int, day int, weekofyear int, \
                                                           month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = (""" INSERT INTO songplays (song_id, artist_id,\
-                                                    time_stamp, user_id, level,\
+                                                    time_start, user_id, level,\
                                                     session_id, location, user_agent) \
                              VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                             
 """)
 
 user_table_insert = (""" INSERT INTO users (user_id, first_name,\
                                             last_name, gender, level) \
                          VALUES (%s, %s, %s, %s, %s)
+                         ON CONFLICT (user_id) DO NOTHING
 """)
 
 song_table_insert = (""" INSERT INTO songs (song_id, title, artist_id , \
                                             year, duration) \
                          VALUES (%s, %s, %s, %s, %s)
+                         ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = (""" INSERT INTO artists (artist_id, name, location, \
                                                 latitude, longitude) \
                            VALUES (%s, %s, %s, %s, %s)
+                           ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = (""" INSERT INTO time (time_stamp, hour, day, weekofyear, \
+time_table_insert = (""" INSERT INTO time (time_start, hour, day, weekofyear, \
                                            month, year, weekday) \
                         VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
